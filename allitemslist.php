8832fab68469b18026b2d420283b51535855ac8a
juanleonel <juanl.albores.c@gmail.com>
2020-04-04 20:51:21 -0500

    view for product added

--- allitemslist.php
+++ allitemslist.php
@@ -14,7 +14,6 @@
 
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
     foreach ($data as $row) {
       echo ' <div class="col-lg-4">
                 <img class="img-circle" src="' . $row['image_name'] . '" alt="Generic placeholder image" width="140" height="140">
@@ -22,12 +21,10 @@
                 <p>' . $row['description'] . '</p>
                 <p>' . '$' . number_format( $row['price'] ). '</p>
                 <p>
-                  <a class="btn btn-default" href="itemdetails.php?itemcode=' . $row['category_href'] . '" role="button">View details &raquo;</a>
+                  <a class="btn btn-default" href="itemdetails.php?Id=' . $row['Id'] . '" role="button">View details &raquo;</a>
                 </p>
               </div>
             ';
-
-      echo "";
     }
 
     $pdo = null;
---
+++ finditem.php
@@ -0,0 +1,71 @@
+<?php
+     include_once 'conn/conn.php';
+    
+
+    if ($_REQUEST && isset($_REQUEST['Id'])) {
+        
+        $id =  $_REQUEST['Id'];        
+       
+        $sql = 'CALL spSelProductDetails(?)';
+        
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(1, $id);
+
+        $stmt->execute();
+
+        $product = $stmt->fetch(PDO::FETCH_ASSOC);
+
+       // var_dump($product);
+
+        if ($product) {
+            
+            echo '
+                    <div class="col-md-4 col-sm-4">
+                        <div  class="thumbnail">
+                            <img src="https://www.am-design.es/Content/img/Bootstrap1.png" alt="...">
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-sm-8">
+                        <form method="post" action="" >
+                            <input type="hidden" value="' .$product['Id']. '">
+                            <div class="thumbnail">                        
+                                <h1>' . $product['product_name'] . '</h1>                       
+                                <div class="caption">
+                                    <h3>' . $product['category_name'] . '</h3>
+                                    <p>' . $product['description'] . '</p>
+                                    <p>' . $product['item_code'] . ',' . $product['weight'] . '</p>
+                                    <div class="">
+                                        <label class=""> Quantity</label>
+                                        <div class="">
+                                            <input class="" type="number">
+                                        </div>                                    
+                                    </div>                                    
+                                        <label class=""> Price: </label> <strong>  $ ' . number_format($product['price']) . ' </strong>                                
+                                    <p>
+                                    <button type="submit" class="btn btn-default">Buy Now</button>
+                                    <button type="submit" class="btn btn-primary">Add Cart</button>                                    
+                                    
+                                    </p>            
+                            </div>                  
+                        </form>
+                    </div>
+                ';
+        }else{
+            echo '
+            <div class="alert alert-warning alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <strong>Warning!</strong> Any result
+            </div>';
+        }
+    }else{
+      
+        echo '
+        <div class="alert alert-warning alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>Warning!</strong> Any result
+        </div>';
+    }
+
+
+?>
